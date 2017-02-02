7bf875ce491795c9cdc2d001a81ae8059898011d
Christopher Petty <christopher.m.petty@gmail.com>
2017-02-02 15:47:32 +1300

    globalStyles.js

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,14 @@ import { App } from './App.jsx';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { globalStyles } from './globalStyles.js';
 
 // Needed for onTouchTap, won't be required once official React version released.
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
 // Apply app wide styles here (http://www.material-ui.com/#/customization/themes)
-const muiTheme = getMuiTheme({
-
-});
+const muiTheme = getMuiTheme(globalStyles);
 
 const StyledApp = () => (
   <MuiThemeProvider muiTheme={muiTheme}>
