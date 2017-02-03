0e3ea4964f24c51f3d4e989ae2cf185d0bf78844
Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
2017-02-03 17:22:11 +1300

    Revert "react-router setup"

--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,22 @@
 import React from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-
-import AboutRoute from './routes/about';
-import ApiRoute from './routes/api';
-import LegendRoute from './routes/legend';
-import App from './components/App.jsx';
-
-// routesConfig
-const routesConfig = {
-  path: '/',
-  component: App,
-  // indexRoute: {component: Home},
-  childRoutes: [
-    AboutRoute,
-    ApiRoute,
-    LegendRoute,
-  ],
-};
+import { App } from './App.jsx';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { globalStyles } from './globalStyles.js';
 
 // Needed for onTouchTap, won't be required once official React version released.
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-render((
-  <Router
-    history={browserHistory}
-    routes={routesConfig}
-  />
-), document.getElementById('root'));
+// Apply app wide styles here (http://www.material-ui.com/#/customization/themes)
+const muiTheme = getMuiTheme(globalStyles);
+
+const StyledApp = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <App />
+  </MuiThemeProvider>
+);
+
+render(<StyledApp />, document.getElementById('root'));
