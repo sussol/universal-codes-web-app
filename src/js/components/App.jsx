cdec9f2a02ab5dd1ee939d5b77af5d55ab165e61
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-02-06 09:26:39 +0545

    [router-setup] added @debug path for result links

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { Link } from 'react-router';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { globalStyles } from '../globalStyles.js';
 
 import Header from './Header.jsx';
+import { globalStyles } from '../globalStyles.js';
 
 // Apply app wide styles here (http://www.material-ui.com/#/customization/themes)
 const muiTheme = getMuiTheme(globalStyles);
@@ -13,6 +14,13 @@ const App = (props) => (
     <MuiThemeProvider muiTheme={muiTheme}>
       <Header {...props} />
     </MuiThemeProvider>
+
+    {/* @delete-me: debug a result path*/}
+    <div>FAKE TABLE</div>
+    <div>@DEBUG: <Link to={'/amoxicillin/am500'}>I link to a result, w00t!</Link></div>
+    <div>@DEBUG: <Link to={'/hydrochlorothiazide/hy50'}>I link to another result, w00t!</Link></div>
+    <div>@DEBUG: <Link to={'/metaformin/me1000'}>I link to  yet another result, w00t!</Link></div>
+    {/* end @debug */}
 
     {props.children ?
       <div className={'box-content mui-shadow'}>
