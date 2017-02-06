8bd23ac8a3fd776b3c3a7d36283d2da3c6fda961
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-02-06 14:43:19 +0545

    [router-setup] adds Search (home) component

---
+++ src/js/components/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Search = (/* props */) => (
+    <div>
+      Search field goes here
+    </div>
+);
+
+Search.propTypes = {
+  params: React.PropTypes.object,
+};
+
+export default Search;
