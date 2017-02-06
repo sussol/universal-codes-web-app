dae44ba47aad8ff16a1f94978db6353c7eb708a1
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-02-06 11:03:07 +0545

    [router-setup] adds MuiThemeProvider to root

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 
 import AboutRoute from './routes/about';
 import ApiRoute from './routes/api';
+import { globalStyles } from './globalStyles.js';
 import LegendRoute from './routes/legend';
 import ResultRoute from './routes/result';
 import App from './components/App.jsx';
+
+// Apply app wide styles here (http://www.material-ui.com/#/customization/themes)
+const muiTheme = getMuiTheme(globalStyles);
 
 // routesConfig
 const routesConfig = {
@@ -27,8 +33,10 @@ const routesConfig = {
 injectTapEventPlugin();
 
 render((
-  <Router
-    history={browserHistory}
-    routes={routesConfig}
-  />
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <Router
+      history={browserHistory}
+      routes={routesConfig}
+    />
+  </MuiThemeProvider>
 ), document.getElementById('root'));
