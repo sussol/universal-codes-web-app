00c0dd833703ff92fefebd1f338582f188f56051
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-02-06 10:59:10 +0545

    [router-setup] adds back button to Result component

--- src/js/routes/result/Result.jsx
+++ src/js/routes/result/Result.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import BackButton from '../../components/BackButton.jsx';
+
 const Result = (props) => (
     <div>
+      <div>
         <b>Drug Name:</b> {props.params.name}<br />
         <b>Drug Code:</b> {props.params.code}
+      </div>
+      <br />
+      <BackButton {...props} />
     </div>
 );
 
