95143ff7f0f79cf2824e1a74680695f89e74f10a
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-02-06 11:01:33 +0545

    [router-setup] abstracts MuiThemeProvider up to parent component Router

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { Link } from 'react-router';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Header from './Header.jsx';
-import { globalStyles } from '../globalStyles.js';
-
-// Apply app wide styles here (http://www.material-ui.com/#/customization/themes)
-const muiTheme = getMuiTheme(globalStyles);
 
 const App = (props) => (
   <div>
-    <MuiThemeProvider muiTheme={muiTheme}>
-      <Header {...props} />
-    </MuiThemeProvider>
+    <Header {...props} />
 
     {/* @delete-me: debug a result path*/}
     <div>FAKE TABLE</div>
-    <div>@DEBUG: <Link to={'/amoxicillin/am500'}>I link to a result, w00t!</Link></div>
-    <div>@DEBUG: <Link to={'/hydrochlorothiazide/hy50'}>I link to another result, w00t!</Link></div>
-    <div>@DEBUG: <Link to={'/metaformin/me1000'}>I link to  yet another result, w00t!</Link></div>
+    <div>
+      @DEBUG: <Link to={'/amoxicillin/am500'}>I link to a result, w00t!</Link>
+    </div>
+    <div>@DEBUG:
+      <Link to={'/hydrochlorothiazide/hy50'}>I link to another result, w00t!</Link>
+    </div>
+    <div>@DEBUG:
+      <Link to={'/metaformin/me1000'}>I link to  yet another result, w00t!</Link>
+    </div>
+    <br />
+    <div><Link to={'/all'}>SHOW ALL RESULTS</Link></div>
     {/* end @debug */}
 
     {props.children ?
