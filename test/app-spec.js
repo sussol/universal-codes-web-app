76f5cd84d01255a4580c890e2d4dbf5839d90ea7
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-02-07 10:43:53 +0545

    [test-bare-setup] renames app-spec.js -to-> app.test.js for Jest; edits init test

--- test/app-spec.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import test from 'blue-tape';
-import { shallow } from 'enzyme';
-
-// REACT COMPONENTS RENDER
-test('should render ____ component', (t) => {
-
-});
---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+/* global test,expect true */
+
+// REACT COMPONENTS RENDER
+
+/**
+* @delete-me
+* And we begin with a proper, failing test ;-)
+**/
+test('expects JavaScript to be Java', () => {
+  expect('JavaScript').toBe('Java');
+});
