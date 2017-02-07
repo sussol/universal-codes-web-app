ec5ef1c658b2012530a3cd9ae332539d6fe22869
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-02-07 14:13:39 +0545

    [router-setup] updates App to show basic home screen; removes debug stuff

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,39 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router';
 
 import Header from './Header.jsx';
 
-const App = (props) => (
+export const App = (props) => (
   <div>
     <Header {...props} />
 
-    {/* @delete-me: debug a result path*/}
-    <div>FAKE TABLE</div>
-    <div>
-      @DEBUG: <Link to={'/amoxicillin/am500'}>I link to a result, w00t!</Link>
-    </div>
-    <div>@DEBUG:
-      <Link to={'/hydrochlorothiazide/hy50'}>I link to another result, w00t!</Link>
-    </div>
-    <div>@DEBUG:
-      <Link to={'/metaformin/me1000'}>I link to  yet another result, w00t!</Link>
-    </div>
-    {/* end @debug */}
-
+    {/* show children components as we navigate */}
     {props.children ?
       <div className={'box-content mui-shadow'}>
       {props.children}
     </div> :
     null}
-
-    {/* show all results */}
-    <br />
-    <div><Link to={'/all'}>SHOW ALL RESULTS</Link></div>
   </div>
 );
 
 App.propTypes = {
   children: React.PropTypes.object,
 };
-
-export default App;
