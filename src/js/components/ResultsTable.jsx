bbc9a45052675ecd9736bdead5df34daac8d7a5b
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-02-15 07:53:04 +0545

    [search-results] add search term to results title

--- src/js/components/ResultsTable.jsx
+++ src/js/components/ResultsTable.jsx
@@ -5,31 +5,18 @@ const columns = [
   {
     key: 'name',
     title: 'Name',
-    sortable: true,
   },
   {
     key: 'code',
     title: 'Code',
-    sortable: true,
-  },
-  {
-    key: 'group',
-    title: 'Editable Column',
-    editable: true,
   },
 ];
-
-// @todo fetch proper data
-const data = [];
-for (let i = 0; i < 100; i++) {
-  data.push({ code: `code${i}`, name: `name${i}`, group: `group${i % 2}` });
-}
 
 export class ResultsTable extends PureComponent {
   render() {
     return (
       <div>
-        <h2>Results</h2>
+        <h2>Results for {this.props.searchTerm}</h2>
         <SussolReactTable
           columns={columns}
           tableData={this.props.data}
