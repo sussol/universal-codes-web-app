edad7963760e60c218737221997129ef7f81fc0b
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-03-28 12:20:51 +0545

    [page-api] some api page text

--- src/js/routes/api/Api.jsx
+++ src/js/routes/api/Api.jsx
@@ -6,7 +6,10 @@ export class Api extends PureComponent {
       <div>
         <div>
           <h2>API Docs</h2>
-          <p>Auto-generated documentation from some source.</p>
+          <p>
+            If you have need to access the Universal Drug Codes from your own app
+            we suggest using our public <a href="https://sussol.github.io/universal-drug-codes-server/">REST API.</a>
+          </p>
         </div>
       </div>
     );
