52042ea9a65a2e4d54dadc5cf7a7ee2a07ff8cba
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-03-28 09:56:21 +0545

    [build-config] updates webpack for dev and upstream

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,28 @@
 /* eslint-disable no-var */
 /* eslint-disable prefer-template */
 var webpack = require('webpack');
-var PROD = process.env.NODE_ENV === 'production';
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var env = process.env.NODE_ENV || 'development';
+var isProd = process.env.NODE_ENV === 'production';
 
 // getEntrySources gets sources for webpack entry paths, by environment
 function getEntrySources(sources) {
-  if (process.env.NODE_ENV !== 'production') {
+  if (!isProd) {
     sources.push('webpack-dev-server/client?http://localhost:8080');
     sources.push('webpack/hot/only-dev-server');
   }
-
   return sources;
 }
 
 module.exports = {
   entry: getEntrySources(['babel-polyfill', './src/js/index.js']),
   output: {
-    publicPath: PROD ? './build/' : 'http://localhost:8080/build/',
+    publicPath: !isProd && 'http://localhost:8080/build/',
     path: __dirname + '/build',
     filename: 'bundle.js',
   },
-  devtool: PROD ? 'cheap-module-source-map' : 'eval',
+  devtool: isProd ? 'cheap-module-source-map' : 'eval',
   module: {
     preLoaders: [
       {
@@ -50,11 +52,22 @@ module.exports = {
     ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+      env: env,
+    }),
     new webpack.ProvidePlugin({
       fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch',
     }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      'process.env.NODE_ENV': JSON.stringify(env),
     }),
   ],
+  stats: {
+    warnings: false,
+  },
+  devServer: {
+    contentBase: './build',
+  },
 };
