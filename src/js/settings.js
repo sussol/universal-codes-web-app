e45a93302eb421f451bbb71483c77d466e6ed6c8
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-04-11 14:37:26 +0545

    [column-widths] moves column widths to settings

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,4 +1,4 @@
-const hostByEnv = (env = process.env.NODE_ENV) => {
+const _hostByEnv = (env = process.env.NODE_ENV) => {
   if (env === 'production') {
 	// @todo change on API launch
     return 'yadaiamy6e.execute-api.ap-southeast-2.amazonaws.com/dev';
@@ -6,6 +6,10 @@ const hostByEnv = (env = process.env.NODE_ENV) => {
   return 'yadaiamy6e.execute-api.ap-southeast-2.amazonaws.com/dev';
 };
 
-export const SERVER_HOST = hostByEnv();
+// set fixed widths for any columns
+// e.g. column 1 should be 150px
+export const ColumnFixedWidths = { 1: 150 };
+
+export const SERVER_HOST = _hostByEnv();
 // local will call dev upstream
 export const SEARCH_URL = `https://${SERVER_HOST}/items?search=`;
