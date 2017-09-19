79c2556bf0c3b7b3137b7c01c3a5c57cdc4c1617
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-09-19 17:33:39 +0545

    [issue-55] adds tests for issue-55

---
+++ src/js/__tests__/Search.spec.js
@@ -0,0 +1,26 @@
+/* eslint-disable new-cap */
+import React from 'react';
+
+import { MuiShallowWithContext } from '../_testUtils';
+import { Search } from '../components/Search';
+
+test('App component can render children', () => {
+  const component = MuiShallowWithContext(<Search />);
+  expect(component.length).toBe(1);
+});
+
+test('Snackbar is rendered', () => {
+  const component = MuiShallowWithContext(<Search />);
+  expect(component.find('Snackbar').length).toBe(1);
+});
+
+test('Snackbar is rendered with initial "open" prop === "false"', () => {
+  const component = MuiShallowWithContext(<Search />);
+  expect(component.find('Snackbar').props().open).toBe(false);
+});
+
+test('Snackbar "open" prop is "true" when no results', () => {
+  const component = MuiShallowWithContext(<Search />);
+  component.setState({ resultData: [], searchTerm: 'axsdd' });
+  expect(component.find('Snackbar').props().open).toBe(true);
+});
