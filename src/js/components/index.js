82025d2a9cd3b036d9d03580cfe6725b0ef991c4
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-10-11 15:11:22 +0545

    [issue-64] do not load ResultsTable from components/index

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -3,6 +3,5 @@ export * from './Api';
 export * from './Header';
 export * from './Legend';
 export * from './Menu';
-export * from './ResultsTable';
 export * from './Search';
 export * from './SearchBar';
